669acbafd5a6493cdee2fdd72335c42c3d3b29b2
yangyinglong <yangyl96@foxmail.com>
2018-10-17 17:07:28 +0800

    框架初步搭建

---
+++ src/main/java/com/yylc/learnLSB/Application.java
@@ -0,0 +1,20 @@
+package com.yylc.learnLSB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class Application extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
+}
---
+++ src/main/java/com/yylc/learnLSB/controller/config/JerseyConfig.java
@@ -0,0 +1,23 @@
+package com.yylc.learnLSB.controller.config;
+
+import org.glassfish.jersey.servlet.ServletContainer;
+import org.glassfish.jersey.servlet.ServletProperties;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置Jersey
+ *
+ */
+@Configuration
+public class JerseyConfig {
+
+	@Bean
+	public ServletRegistrationBean jerseyServlet() {
+		ServletRegistrationBean registration = new ServletRegistrationBean(new ServletContainer(), "/api/*");
+		// our rest resources will be available in the path /rest/*
+		registration.addInitParameter(ServletProperties.JAXRS_APPLICATION_CLASS, JerseyResourceConfig.class.getName());
+		return registration;
+	}
+}
